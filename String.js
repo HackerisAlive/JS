6a6d5d6b08e78c30df47e5a1d17072251ac361c3
HackerisAlive <98787245+HackerisAlive@users.noreply.github.com>
2022-08-20 19:33:19 +0530

    Create String.js

---
+++ String.js
@@ -0,0 +1,14 @@
+var str = "Ayush is good boy and Ayush is Nice"
+console.log(str)
+var num = console.log(str.length)
+console.log(str[1] = "7") // doesn't work
+console.log(str.charAt(1))
+console.log(str.substring(0, ))
+console.log(str.slice(-num, -30))
+console.log(str)
+console.log(str.replace(/ayush/i, "HackerisAlive")) // /i is case insensitive
+console.log(str.replace(/Ayush/g, "HackerisAlive")) // /g is global match
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+//A string can be converted to an array with the split() method
+console.log(str.split("is"))
